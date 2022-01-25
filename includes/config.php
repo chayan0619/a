6f73ba2168d00b846b258c2e0e0c0cae6a7b3ceb
chayan0619 <52231215+chayan0619@users.noreply.github.com>
2022-01-25 20:10:14 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'webapp1-server1.mysql.database.azure.com', 'server1', 'Chayan@1999', 'webapp', 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, 'webapp1-server1.mysql.database.azure.com', 'server1', 'Chayan@1999', 'webapp1', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
