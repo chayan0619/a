be4a3a3cf95ebbdc842cd983d02f200fc89a2063
chayan0619 <52231215+chayan0619@users.noreply.github.com>
2022-01-25 19:52:16 +0530

    Create try.php

---
+++ try.php
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <form action="login.php" method="post">
+    name:<input type="text" name="name"/>
+      <input type="submit" name="sb"/>
+    </form>
+      </body>
+    </html>
