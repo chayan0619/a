ca12149fd41bf8597139e0418976db6a834fd2d6
chayan0619 <52231215+chayan0619@users.noreply.github.com>
2022-01-25 19:55:08 +0530

    Create try2.php

---
+++ try2.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require_once('includes/config.php');
+if(isset($_POST['sb']))
+{
+  $name=$_POST['name'];
+   
+  $run = mysqli_query($conn,"insert into user (name) values('$name')");
+  if($run)
+  {
+    echo" inserted value";
+  }
+  else
+  {
+    echo "value not inserted";
+  }
+  
+}
+mysqli_close($conn);
+?>
