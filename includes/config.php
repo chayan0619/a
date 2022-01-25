c33bbc93d3f5a2d3b569d44a390eb55a3e0a5e3f
chayan0619 <52231215+chayan0619@users.noreply.github.com>
2022-01-25 16:47:15 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,22 @@
 <?php
 
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'webapp1-server1.mysql.database.azure.com', 'server1', 'Chayan@1999', 'webapp1', 3306 , MYSQLI_CLIENT_SSL);
+$host = 'webapp1-server1.mysql.database.azure.com';
+$username = 'server1';
+$password = 'Chayan@1999';
+$db_name = 'webapp1';
 
-if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
+//Initializes MySQLi
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, 'webapp1-server1.mysql.database.azure.com', 'server1', 'Chayan@1999', 'loginsystem', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 ?>
