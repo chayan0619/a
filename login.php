f08246b383c16b24dd01b0c83f3029e588c5cb5b
chayan0619 <52231215+chayan0619@users.noreply.github.com>
2022-01-26 15:22:03 +0530

    Update login.php

--- login.php
+++ login.php
@@ -41,7 +41,7 @@ echo "<script>alert('Invalid username or password');</script>";
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>User Login | Registration and Login System</title>
+        <title>User Login | Student Login System</title>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
