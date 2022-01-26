e6ea53ed07c3f18df3cc05be906bf8952264429b
chayan0619 <52231215+chayan0619@users.noreply.github.com>
2022-01-26 13:18:08 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,5 +1,14 @@
 <?php 
 echo "hello";
-require_once('includes/config.php');
+
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, 'webapp1-server1.mysql.database.azure.com', 'server1', 'Chayan@1999', 'loginsystem', 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
 
 ?>
