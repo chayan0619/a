c1af5e680315b43c317d2cb5544b53309e12da91
chayan0619 <52231215+chayan0619@users.noreply.github.com>
2022-01-26 14:42:35 +0530

    Delete try.php

--- try.php
+++
@@ -1,8 +0,0 @@
-<html>
-  <body>
-    <form action="try2.php" method="post">
-    name:<input type="text" name="name"/>
-      <input type="submit" name="sb"/>
-    </form>
-      </body>
-    </html>
